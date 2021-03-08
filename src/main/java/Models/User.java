8f55b42f9cc758e33d0d9f9a4d7799fa6771740f
showboat051 <rashad.davis87@yahoo.com>
2021-03-08 00:08:58 -0600

    added user class

---
+++ src/main/java/Models/User.java
@@ -0,0 +1,42 @@
+package Models;
+
+public class User {
+
+
+    String firstName;
+    String lastName;
+    String clearance;
+
+    public User(String firstName, String lastName, String clearance) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.clearance = clearance;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getClearance() {
+        return clearance;
+    }
+
+    public void setClearance(String clearance) {
+        this.clearance = clearance;
+    }
+
+
+
+}
